d95553ee8e35bee3a3c1f229713cdec9b5f8c57f
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-04-13 10:44:26 +0200

    Do not panic when running with no arguments

--- arguments/arguments.go
+++ arguments/arguments.go
@@ -5,5 +5,9 @@ import "os"
 func GetArguments() string {
 	argsWithoutProgramName := os.Args[1:]
 
-	return argsWithoutProgramName[0]
+	if len(argsWithoutProgramName) == 1 {
+		return argsWithoutProgramName[0]
+	}
+
+	return ""
 }
