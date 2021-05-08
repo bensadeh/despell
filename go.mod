d7db361f38d5f45375097869bdcae5b776217911
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2021-05-08 17:41:27 +0200

    Revert "Add cobra"
    This reverts commit 4faa4348c84e9d3f7af5177c731a0631c481e9ec.

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module despell
 
 go 1.16
-
-require github.com/spf13/cobra v1.1.3
--- main.go
+++ main.go
@@ -4,13 +4,9 @@ import (
 	"despell/arguments"
 	"despell/icons"
 	"fmt"
-
-	"github.com/spf13/cobra/cobra/cmd"
 )
 
 func main() {
-	_ = cmd.Execute()
-
 	arg := arguments.GetArguments()
 	icon := icons.GetIcon(arg)
 
