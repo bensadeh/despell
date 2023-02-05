e97c8c72813b16cbfbbbaac96d7648889114d7f6
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-02-05 11:18:30 +0100

    Set unknown command to default mapping

--- unknown/unknown.go
+++ unknown/unknown.go
@@ -1,11 +1,16 @@
 package unknown
 
-import "github.com/bensadeh/despell/core"
+import (
+	"github.com/bensadeh/despell/colors"
+	"github.com/bensadeh/despell/core"
+	"github.com/bensadeh/despell/emoji"
+	"github.com/bensadeh/despell/nerdfont"
+)
 
 const (
 	MissingCommandKey = "unknownCommand"
 )
 
 func GetUnknownCommandIcon() core.Icon {
-	return core.Icon{Icon: "", Color: "gray", Emoji: "◽️"}
+	return core.Icon{Icon: nerdfont.Shell, Color: colors.Normal, Emoji: emoji.TopHat}
 }
