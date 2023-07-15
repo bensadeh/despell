0ce6f7254581aa08628d0b9f9b16e6b9a93998a4
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-07-15 09:55:09 +0200

    Wire up everything

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crate::icon::Icon;
 use clap::Parser;
 
 mod color;
@@ -15,12 +16,12 @@ struct Args {
     cmd_name: String,
 
     /// Let despell override the color of the icon
-    #[clap(short = 'c', long = "color", conflicts_with = "emoji")]
-    color: bool,
+    #[clap(short = 'c', long = "color", conflicts_with = "use_emoji")]
+    use_color: bool,
 
     /// Use emojis instead of nerdfonts
-    #[clap(short = 'e', long = "emoji", conflicts_with = "color")]
-    emoji: bool,
+    #[clap(short = 'e', long = "emoji", conflicts_with = "use_color")]
+    use_emoji: bool,
 
     /// Use custom mappings from ~/.config/despell/config.toml
     #[clap(short = 'u', long = "custom")]
@@ -38,7 +39,19 @@ fn main() {
         defaults::get_icon(cmd_name).unwrap_or_default()
     };
 
-    println!("Nerdfont: {}", icon.nerdfont);
-    println!("Color: {}", icon.color);
-    println!("Emoji: {}", icon.emoji);
+    let output = get_output(args.use_emoji, args.use_color, icon);
+
+    print!("{}", output);
+}
+
+fn get_output(use_emoji: bool, use_color: bool, icon: Icon) -> String {
+    if use_emoji {
+        return icon.emoji;
+    }
+
+    if use_color {
+        return format!("#[fg={}]{}", icon.color, icon.nerdfont);
+    }
+
+    icon.nerdfont
 }
