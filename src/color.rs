ccd8551605ef1affc9d35f801a9da4f696781a77
Ben Sadeh <701096+bensadeh@users.noreply.github.com>
2023-08-11 08:18:31 +0200

    Use string literal

--- src/color.rs
+++ src/color.rs
@@ -33,8 +33,8 @@ impl<'de> Deserialize<'de> for Color {
     where
         D: Deserializer<'de>,
     {
-        let color_str = String::deserialize(deserializer)?;
-        match color_str.as_str() {
+        let color: &str = Deserialize::deserialize(deserializer)?;
+        match color {
             "none" => Ok(Color::None),
             "red" => Ok(Color::Red),
             "green" => Ok(Color::Green),
@@ -45,7 +45,7 @@ impl<'de> Deserialize<'de> for Color {
             "orange" => Ok(Color::Orange),
             _ => Err(serde::de::Error::custom(format!(
                 "Invalid color: {}",
-                color_str
+                color
             ))),
         }
     }
